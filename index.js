45899eb712c72ea143d818160b5c8ac3e4c2fcac
Kranthi Prasad <kranthichintu2004@gmail.com>
2026-02-11 22:16:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 require("dotenv").config();
+const express = require("express");
 const connectDB = require("./db");
 const bot = require("./bot");
 const { startScheduler } = require("./scheduler");
 
+const app = express();
+
+// REQUIRED FOR RENDER
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("College Timetable Bot is Running 🚀");
+});
+
 async function startApp() {
   await connectDB();
   startScheduler(bot);
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 }
 
 startApp();
